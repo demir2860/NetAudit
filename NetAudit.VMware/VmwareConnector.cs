0d3e290e12ee6edace5505661893cffef6e5b6ff
demir2860 <ozkan.demir@teknolojimimari.com>
2026-07-17 12:07:01 +0300

    Add NetAudit.VMware/VmwareConnector.cs

---
+++ NetAudit.VMware/VmwareConnector.cs
@@ -0,0 +1,9 @@
+namespace NetAudit.VMware;
+
+public class VmwareConnector
+{
+    public string Connect(string host, string username, string password)
+    {
+        return "VMware connector placeholder - integration not yet implemented";
+    }
+}
